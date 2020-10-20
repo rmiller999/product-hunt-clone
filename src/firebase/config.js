0da4cb2f68042b6d6c48ea34c5d33d8f2a57058b
Reid Miller <reidmiller999@gmail.com>
2020-10-19 22:20:54 -0700

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBY3TUFlnNQFPIVsyVv_cD3q9bkKpbi9Z0",
+  authDomain: "product-hunt-ffe80.firebaseapp.com",
+  databaseURL: "https://product-hunt-ffe80.firebaseio.com",
+  projectId: "product-hunt-ffe80",
+  storageBucket: "product-hunt-ffe80.appspot.com",
+  messagingSenderId: "811276851586",
+  appId: "1:811276851586:web:358c15f7295469f2b2979b",
+};
+
+export default firebaseConfig;
