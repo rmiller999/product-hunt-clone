086a44d6e95b02d213e0526c889a204c846dd87c
Reid Miller <reidmiller999@gmail.com>
2020-10-19 21:35:40 -0700

    Routing and Tabs

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { IonPage, IonHeader, IonToolbar, IonTitle } from "@ionic/react";
+
+const Home = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Hunt</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
+};
+export default Home;
