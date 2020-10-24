b186e746bf6c80344bc150b1a4fce82ea033f108
Reid Miller <reidmiller999@gmail.com>
2020-10-23 18:49:10 -0700

    Reset Password Page and Validation

--- src/pages/Forgot.js
+++ src/pages/Forgot.js
@@ -10,20 +10,49 @@ import {
   IonButton,
 } from "@ionic/react";
 import NavHeader from "../components/Header/NavHeader";
+import { toast } from "../utils/toast";
+import useFormValidation from "../hooks/useFormValidation";
+import validatePasswordReset from "../components/Auth/validatePasswordReset";
+import firebase from "../firebase";
 
-const Forgot = () => {
+const INITIAL_STATE = {
+  email: "",
+};
+
+const Forgot = (props) => {
+  const {
+    handleSubmit,
+    handleChange,
+    values,
+    isSubmitting,
+  } = useFormValidation(INITIAL_STATE, validatePasswordReset, handleResetPassword);
+  const [busy, setBusy] = React.useState(false);
+
+  async function handleResetPassword() {
+    setBusy(true);
+    const { email } = values;
+    try {
+      await firebase.resetPassword(email);
+      toast("Check your email to reset your password");
+      props.history.push("/login");
+    } catch (err) {
+      console.error("Password Reset Error", err);
+      toast(err.message);
+    }
+    setBusy(false);
+  }
   return (
     <IonPage>
       <NavHeader title="Reset Password" />
       <IonContent>
         <IonItem lines="full">
           <IonLabel position="floating">Email</IonLabel>
-          <IonInput name="email" type="text" required></IonInput>
+          <IonInput name="email" value={values.email} type="text" onIonChange={handleChange} required></IonInput>
         </IonItem>
 
         <IonRow>
           <IonCol>
-            <IonButton type="submit" color="primary" expand="block">
+            <IonButton type="submit" color="primary" expand="block" onClick={handleSubmit} disabled={isSubmitting}>
               Get Reset Link!
             </IonButton>
           </IonCol>
